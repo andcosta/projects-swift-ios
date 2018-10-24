ec8f87ea7330b05c1667382e9dae6cb2ab8924ed
Anderson Carvalho <anderson.analistati@gmail.com>
2018-10-24 05:05:42 -0300

    AppDelegate, FBMeBase

---
+++ Project 03/FacebookMe/FBMeBaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FBMeBaseViewController.swift
+//  FacebookMe
+//
+//  Copyright © 2017 Anderson Costa. All rights reserved.
+//
+
+import UIKit
+
+class FBMeBaseViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = Specs.color.gray
+  }
+}
