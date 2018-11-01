2894ef2f325d4c471786212267b35c70c283892b
Anderson Costa <anderson.analistati@gmail.com>
2018-11-01 09:40:27 -0300

    Cell

---
+++ Project 05/Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Anderson Costa.
+//  Copyright © 2018. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+  
+}
