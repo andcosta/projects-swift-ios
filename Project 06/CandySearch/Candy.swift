0122dbb63217868512c818d2bdd0a0a6d41c7895
Anderson Costa <anderson.analistati@gmail.com>
2018-11-07 02:07:14 -0200

    Project

---
+++ Project 06/CandySearch/Candy.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+
+class Candy {
+  var category : String
+  var name : String
+  
+  init(category: String, name: String) {
+    self.category = category
+    self.name = name
+  }
+}
