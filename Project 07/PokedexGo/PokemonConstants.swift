26a090ecb91814a59699ae88b1a165b1fbb90e74
Anderson Costa <anderson.analistati@gmail.com>
2018-11-15 02:42:44 -0200

    Pokemon

---
+++ Project 07/PokedexGo/PokemonConstants.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonConstants.swift
+//  PokedexGo
+//
+//  Created by Anderson Costa
+//
+
+import Foundation
+
+let pokemons = [
+  Pokemon(name: "pokemon", id: 1,
+    detailInfo: "pokemon",
+    type: [PokeType.grass, PokeType.poison],
+    weak: [PokeType.fire, PokeType.flying, PokeType.ice, PokeType.psychic],
+    pokeImgUrl: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"),
+  
+  Pokemon(name: "pokemon", id: 2,
+    detailInfo: "pokemon",
+    type: [PokeType.grass, PokeType.poison],
+    weak: [PokeType.fire, PokeType.flying, PokeType.ice, PokeType.psychic],
+    pokeImgUrl: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"),
+]
