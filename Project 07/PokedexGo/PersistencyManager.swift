e21eae75882f29d0e27ce2653b25fa61b84b9bd1
Anderson Costa <anderson.analistati@gmail.com>
2018-11-15 02:42:25 -0200

    Master and persistency

---
+++ Project 07/PokedexGo/PersistencyManager.swift
@@ -0,0 +1,27 @@
+//
+//  PersistencyManager.swift
+//  PokedexGo
+//
+//  Created by Anderson Costa
+//
+
+import UIKit
+
+class PersistencyManager: NSObject {
+  func saveImage(_ image: UIImage, filename: String) {
+    let path = NSHomeDirectory() + "/Documents/\(filename)"
+    let data = UIImagePNGRepresentation(image)
+    try? data!.write(to: URL(fileURLWithPath: path), options: [.atomic])
+  }
+  
+  func getImage(_ filename: String) -> UIImage? {
+    let path = NSHomeDirectory() + "/Documents/\(filename)"
+    
+    do {
+      let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .uncachedRead)
+      return UIImage(data: data)
+    } catch {
+      return nil
+    }
+  }
+}
