83bd3ef30f4df918ed4ced1ac4221a3adab6c6d3
Anderson Costa <anderson.analistati@gmail.com>
2018-11-23 00:39:16 -0200

    Project Feed

---
+++ Project 08/SimpleRSSReader/NewsTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  NewsTableViewCell.swift
+//  SimpleRSSReader
+//
+//  Created by Anderson Costa
+//  Copyright (c) 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+enum CellState {
+  case expanded
+  case collapsed
+}
+
+class NewsTableViewCell: UITableViewCell {
+  @IBOutlet weak var titleLabel:UILabel!
+  
+  @IBOutlet weak var descriptionLabel:UILabel! {
+    didSet {
+      descriptionLabel.numberOfLines = 4
+    }
+  }
+  
+  @IBOutlet weak var dateLabel:UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
