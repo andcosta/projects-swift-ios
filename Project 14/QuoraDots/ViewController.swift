2050cf4c9f3396fcbd471dbb3b7900ee19dc9580
Anderson Costa <anderson.analistati@gmail.com>
2019-01-30 22:58:43 -0200

    Project

---
+++ Project 14/QuoraDots/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  QuoraDots
+//
+//  Created by Anderson Costa on 7/11/18.
+//  Copyright © 2018 yigu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var dotOne: UIImageView!
+  @IBOutlet weak var dotTwo: UIImageView!
+  @IBOutlet weak var dotThree: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    startAnimation()
+  }
+
+  func startAnimation() {
+    dotOne.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    dotTwo.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    dotThree.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+      self.dotOne.transform = CGAffineTransform.identity
+    }, completion: nil)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.2, options: [.repeat, .autoreverse], animations: {
+      self.dotTwo.transform = CGAffineTransform.identity
+    }, completion: nil)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.4, options: [.repeat, .autoreverse], animations: {
+      self.dotThree.transform = CGAffineTransform.identity
+    }, completion: nil)
+  }
+}
+
